09e9e941c56e29df92de1a32608468ee079397b1
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2018-12-19 16:24:52 +0900

    jananese keymap

--- keyboards/helix/rev2/keymaps/myhelix/keymap.c
+++ keyboards/helix/rev2/keymaps/myhelix/keymap.c
@@ -60,28 +60,49 @@ enum macro_keycodes {
 //Macros
 #define M_SAMPLE M(KC_SAMPLEMACRO)
 
+#define JA_CLON KC_QUOT  // ' -> : and +
+#define JA_AT   KC_LBRC  // [ -> @ and `
+#define JA_HAT  KC_EQL   // = -> ^ and ~
+#define JA_ENUN KC_RO    // not used -> \ and _ (EN mark and UNder score)
+#define JA_ENVL KC_JYEN  // not userd -> \ and | (EN mark and Vertical Line)
+#define JA_LBRC KC_RBRC  // ] -> [ and {
+#define JA_RBRC KC_BSLS  // \ -> ] and }
+
+#define JA_DQUOT KC_AT   // @ -> "
+#define JA_BQUOT KC_LCBR // " -> `
+#define JA_LCBR KC_RCBR  // } -> {
+#define JA_RCBR KC_PIPE  // | -> }
+#define JA_RPRN KC_LPRN  // ( -> )
+#define JA_TRAS KC_RPRN  // ) -> blank
+#define JA_AMPR KC_CIRC  // ^ -> &
+#define JA_TGGL KC_TILD  // ~ -> toggle en/ja
+#define JA_LPRN KC_ASTR  // * -> (
+#define JA_TILD KC_PLUS  // + -> ~
+#define JA_QUOT KC_AMPR  // & -> '
+
+
 #if HELIX_ROWS == 5
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Qwerty
    * ,-----------------------------------------.             ,-----------------------------------------.
-   * |   `  |   1  |   2  |   3  |   4  |   5  |             |   6  |   7  |   8  |   9  |   0  | Del  |
+   * | Esc  |   1  |   2  |   3  |   4  |   5  |             |   6  |   7  |   8  |   9  |   0  |  -   |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * | Tab  |   Q  |   W  |   E  |   R  |   T  |             |   Y  |   U  |   I  |   O  |   P  | Bksp |
+   * | Tab  |   Q  |   W  |   E  |   R  |   T  |             |   Y  |   U  |   I  |   O  |   P  |  @   |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * | Ctrl |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |  '   |
+   * | Ctrl |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |  :   |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Shift|   Z  |   X  |   C  |   V  |   B  |   [  |   ]  |   N  |   M  |   ,  |   .  |   /  |Enter |
+   * | Shift|   Z  |   X  |   C  |   V  |   B  | PGUP |PGDOWN|   N  |   M  |   ,  |   .  |   /  |  \   |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |Adjust| Esc  | Alt  | GUI  | EISU |Lower |Space |Space |Raise | KANA | Left | Down |  Up  |Right |
+   * |Adjust|   `  | Alt  | GUI  |Lower |Space | Bksp | Tab  |Enter |Raise | Left |  Up  | Down |RIGHT |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = KEYMAP( \
-      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-      ADJUST,  KC_ESC,  KC_LALT, KC_LGUI, EISU,    LOWER,   KC_SPC,  KC_SPC,  RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, \
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    JA_AT, \
+      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, JA_CLON, \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_PGUP, KC_PGDN,  KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, JA_ENUN , \
+      ADJUST,  KC_GRV,  KC_LALT, KC_LGUI, LOWER,  KC_SPC,  KC_BSPC,  KC_TAB, KC_ENT,  RAISE,   KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT \
       ),
 
   /* Colemak
@@ -150,21 +171,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Raise
    * ,-----------------------------------------.             ,-----------------------------------------.
    * |   `  |   1  |   2  |   3  |   4  |   5  |             |   6  |   7  |   8  |   9  |   0  | Bksp |
+   * |------+------+---------------------------|             |------+------+------+------+------+------|
+   * |   `  |   1  |   2  |   {  |   }  |  ~   |             |   6  |   7  |   8  |   9  |   0  | Del  |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |   `  |   1  |   2  |   3  |   4  |   5  |             |   6  |   7  |   8  |   9  |   0  | Del  |
-   * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |   -  |   =  |   [  |   ]  |  \   |
+   * |      |      |      |   (  |   )  |  |   |             |  F6  |   -  |   =  |   [  |   ]  |  \   |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |  F7  |  F8  |  F9  |  F10 |  F11 |      |      |  F12 |      |      |PageDn|PageUp|      |
+   * |      |      |      |   [  |   ]  |      |      |      |  F12 |      |      |PageDn|PageUp|      |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * |      |      |      |      |      |      |      |      |      |      | Next | Vol- | Vol+ | Play |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_RAISE] = KEYMAP( \
-      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
-      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-      _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, KC_F12,  _______, _______, KC_PGDN, KC_PGUP, _______, \
+      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,     KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+      KC_GRV,  KC_1,    KC_2,    JA_LCBR, JA_RCBR,JA_TILD,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
+      _______, _______, _______, JA_LPRN, JA_RPRN,JA_ENVL,                     KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
+      _______, _______, _______, JA_LBRC, JA_RBRC, _______, _______, _______, KC_F12,  _______, _______, KC_PGDN, KC_PGUP, _______, \
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
       ),
 
