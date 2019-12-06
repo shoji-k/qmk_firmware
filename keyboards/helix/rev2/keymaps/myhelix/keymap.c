e14445a797ddb91d9b397357b466fc062ee0f168
shoji-k <shoji.kobayashi.0125@gmail.com>
2019-12-06 10:50:58 +0900

    switch alt and gui

--- keyboards/helix/rev2/keymaps/myhelix/keymap.c
+++ keyboards/helix/rev2/keymaps/myhelix/keymap.c
@@ -86,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  | PGUP |PGDOWN|   N  |   M  |   ,  |   .  |   /  |  \   |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |Adjust| GUI  | Alt  | Left |Right |Space | Bksp | Tab  |Enter |  Up  | Down | PGUP |PGDOWN|Raise |
+   * |Adjust| Alt  | GUI  | Left |Right |Space | Bksp | Tab  |Enter |  Up  | Down | PGUP |PGDOWN|Raise |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT( \
@@ -94,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_AT, \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, JA_CLON, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_PGUP, KC_PGDN,  KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, JA_ENUN , \
-      ADJUST,  KC_LGUI, KC_LALT, KC_LEFT, KC_RGHT, KC_SPC,  KC_BSPC,  KC_TAB,KC_ENT,  KC_UP,    KC_DOWN,KC_PGUP, KC_PGDN,   RAISE \
+      ADJUST,  KC_LALT, KC_LGUI, KC_LEFT, KC_RGHT, KC_SPC,  KC_BSPC,  KC_TAB,KC_ENT,  KC_UP,    KC_DOWN,KC_PGUP, KC_PGDN,   RAISE \
       ),
 
   /* Colemak
