58168b37309d957f60ad506dc37aecca01d62d41
shoji-k <shoji.kobayashi.0125@gmail.com>
2019-12-06 13:28:28 +0900

    add combo settings

--- keyboards/helix/rev2/keymaps/myhelix/keymap.c
+++ keyboards/helix/rev2/keymaps/myhelix/keymap.c
@@ -73,6 +73,19 @@ enum macro_keycodes {
 #define JA_TILD KC_PLUS  // + -> ~
 #define JA_QUOT KC_AMPR  // & -> '
 
+enum combos {
+  CC_ESC,
+  AB_ESC
+};
+
+const uint16_t PROGMEM cc_combo[] = {KC_LCTL, KC_ENT, COMBO_END};
+const uint16_t PROGMEM ab_combo[] = {KC_A, KC_B, COMBO_END};
+
+combo_t key_combos[COMBO_COUNT] = {
+  [CC_ESC] = COMBO(cc_combo, KC_ESC), // this works but not quickly
+  [AB_ESC] = COMBO(ab_combo, KC_ESC) // ab is just sample
+};
+
 #if MATRIX_ROWS == 10 // HELIX_ROWS == 5
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -86,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  | PGUP |PGDOWN|   N  |   M  |   ,  |   .  |   /  |  \   |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |Adjust| Alt  | GUI  | Left |Right |Space | Bksp | Tab  |Enter |  Up  | Down | PGUP |PGDOWN|Raise |
+   * |Adjust| GUI  | Alt  | Left |Right |Space | Bksp | Tab  |Enter |  Up  | Down | PGUP |PGDOWN|Raise |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT( \
@@ -94,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_AT, \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, JA_CLON, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_PGUP, KC_PGDN,  KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, JA_ENUN , \
-      ADJUST,  KC_LALT, KC_LGUI, KC_LEFT, KC_RGHT, KC_SPC,  KC_BSPC,  KC_TAB,KC_ENT,  KC_UP,    KC_DOWN,KC_PGUP, KC_PGDN,   RAISE \
+      ADJUST,  KC_LGUI, KC_LALT, KC_LEFT, KC_RGHT, KC_SPC,  KC_BSPC,  KC_TAB,KC_ENT,  KC_UP,    KC_DOWN,KC_PGUP, KC_PGDN,   RAISE \
       ),
 
   /* Colemak
